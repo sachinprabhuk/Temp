07b8d535523b635966dd1618f69378d1ba59a795
sachinprabhuk <prabhusachin44@gmail.com>
2019-10-22 14:21:15 +0530

    init commit

---
+++ functions.js
@@ -0,0 +1,5 @@
+function addNums(a, b) {
+  return a + b + 1;
+}
+
+module.exports.addNums = addNums;
---
+++ main.js
@@ -0,0 +1,3 @@
+const fns = require("./functions");
+
+if (fns.addNums(10, 20) != 30) throw new Error("errorororo");
